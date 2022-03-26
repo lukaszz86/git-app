421adc0951fe81f8453cb2b5c8f5057c29550d0d
lukaszz86 <lukasz.zydziak@gmail.com>
2022-03-26 18:26:36 +0100

    Git-App - refactor GitRepositorySummaryService to run in parallel

--- src/main/java/com/gitapp/services/GitRepositorySummaryService.java
+++ src/main/java/com/gitapp/services/GitRepositorySummaryService.java
@@ -3,10 +3,12 @@ package com.gitapp.services;
 import com.gitapp.model.BranchSummary;
 import com.gitapp.model.GitRepositorySummary;
 import com.gitapp.model.branches.Branch;
+import com.gitapp.model.listing.GitRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 import java.util.List;
 
@@ -19,17 +21,27 @@ public class GitRepositorySummaryService {
     private final GitRepositoryListingService gitRepositoryListingService;
 
     public Flux<GitRepositorySummary> combineGitRepositorySummary(String username){
-
         return gitRepositoryListingService.listUserGitRepositories(username)
                 .filter(gitRepository -> !gitRepository.isFork())
-                .flatMap(gitRepository -> {
-                    String repositoryName = gitRepository.getRepositoryName();
-                    String ownerName = gitRepository.getOwner().getLogin();
-                    return gitBranchesService.retrieveBranches(username, repositoryName)
-                            .flatMap(branch -> buildBranchSummary(branch))
-                            .collectList()
-                            .flatMap(branches -> buildGitRepositorySummary(ownerName,repositoryName,branches));
-                } );
+                .collectList()
+                .flatMapMany(gitRepositories -> runMappingWithBranchesInParallel(username,gitRepositories));
+    }
+
+    private Flux<GitRepositorySummary> runMappingWithBranchesInParallel(String username, List<GitRepository> gitRepositories) {
+        return Flux.fromIterable(gitRepositories)
+                .parallel()
+                .runOn(Schedulers.parallel())
+                .flatMap(gitRepository -> mapRepositoryWithBranches(username, gitRepository))
+                .sequential();
+    }
+
+    private Mono<GitRepositorySummary> mapRepositoryWithBranches(String username, GitRepository gitRepository) {
+        String repositoryName = gitRepository.getRepositoryName();
+        String ownerName = gitRepository.getOwner().getLogin();
+        return gitBranchesService.retrieveBranches(username, repositoryName)
+                .flatMap(branch -> buildBranchSummary(branch))
+                .collectList()
+                .flatMap(branches -> buildGitRepositorySummary(ownerName, repositoryName, branches));
     }
 
     private Mono<BranchSummary> buildBranchSummary(Branch branch) {
